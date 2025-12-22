2f6d2f4cc60f13362b0d19e0e45c78aa3f9d3303
Omar Chatila <omar.chatila@tu-dortmund.de>
2025-12-22 04:14:55 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+#include "src/FibHeap.h"
+
+int main() {
+    FibHeap<int> fb;
+    FibHeap<int> fib_heap;
+    fib_heap.insert(8);
+    fib_heap.insert(2);
+    fib_heap.insert(1);
+    fib_heap.insert(7);
+    fib_heap.insert(2);
+
+    const auto min = fib_heap.extract_min();
+    std::cout << "Min: " << min << std::endl;
+    auto min2 = fib_heap.extract_min();
+    auto min3 = fib_heap.extract_min();
+    std::cout << "Min 2: " << min2 << "\n";
+    std::cout << "Min 3: " << min3 << "\n";
+
+    FibHeap<int> fib_heap2;
+    fib_heap2.insert(3);
+    fib_heap2.insert(9);
+    fib_heap2.insert(5);
+    fib_heap.merge(fib_heap2);
+    std::cout << "min merged" << fib_heap.min() << std::endl;
+
+}
